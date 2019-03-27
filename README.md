d4bfb7fb6471d96c3d513e1f5da8836d18da84f2
harrisonchan9 <44187324+harrisonchan9@users.noreply.github.com>
2019-03-27 18:24:42 +0800

    hi font generator; upt folder structure; tabs

---
+++ README.md
@@ -0,0 +1,42 @@
+# Hiinc Style Baseline and Fonts
+---
+### Index
+
+[0. Getting Started](Getting Started)
+
+[1. Color Palette](Color Palette)
+
+[9. Production Build](Production Build)
+
+
+
+---
+## Getting Started
+### If you are using this library in your project
+...
+
+### If you are contributing
+1. Download or clone this project from git hub
+2. `npm install`
+3. If you don't have `gulp` installed globally, run `npm install gulp -g`
+
+##### Styles and Components
+`gulp watch`
+This will compile `style.scss` to `style.css` live
+
+##### Custom Fonts
+We are converting svg icons into font
+1. Place your svg icon in `/public/images/svg`
+2. Run `gulp svg`
+
+
+## Color Palette
+### 
+
+
+
+
+
+## Production Build
+`gulp production`
+This will compile all file and copy necessary assets to `/dist` folder
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-// const sort = require('gulp-sort');
+const sort = require('gulp-sort');
 // const minify = require('gulp-minify');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const watch = require('gulp-watch');
+
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var runTimestamp = Math.round(Date.now() / 1000);
 
 /*
  *
@@ -15,12 +18,72 @@ const watch = require('gulp-watch');
  */
 sass.compiler = require('node-sass');
 gulp.task('scss', () => {
-  return gulp.src('./stylesheets/style.scss', { sourcemaps: true, allowEmpty: true })
+  return gulp.src('./public/stylesheets/style.scss', { sourcemaps: true, allowEmpty: true })
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest('./dist/stylesheets'));
+    .pipe(gulp.dest('./public/stylesheets'));
 });
 
 gulp.task("watch", () => {
-  gulp.watch("./stylesheets/**/*.scss", gulp.series("scss"));
+  gulp.watch("./public/stylesheets/**/*.scss", gulp.series("scss"));
 });
+
+/*
+ *
+ *  Convert svg files into font file
+ *  Generate class mapping for each svg
+ * 
+ */
+var iconfont = require('gulp-iconfont');
+var consolidate = require('gulp-consolidate');
+var runTimestamp = Math.round(Date.now() / 1000);
+
+var iconStream = gulp.src(['iconFactory/svg/*.svg'])
+  .pipe(sort())
+  .pipe(iconfont({
+    fontName: 'hiStyle',
+    normalize: true,
+    fontHeight: 2000,
+    centerHorizontally: true,
+    prependUnicode: true, // recommended option
+    formats: [ 'ttf', 'eot', 'svg', 'woff', 'woff2' ],
+  }))
+  .on('error', err => {
+    console.log(err)
+  });
+
+// generate scss / css file
+gulp.task("handleGlyphs", done => {
+  iconStream.on('glyphs', (glyphs, options) => {
+    gulp
+      .src(['iconFactory/templates/*.*'])
+      .pipe(sort())
+      .pipe(consolidate('lodash', {
+        glyphs: glyphs,
+        fontName: 'hiStyle',
+        fontPath: 'fonts/hiStyle/',
+        className: 'icon',
+        timestamp: runTimestamp, // recommended to get consistent builds when watching files
+      }))
+      .pipe(gulp.dest('iconFactory/result'))
+      .on('finish', () => {
+        gulp.src(['iconFactory/result/*.scss'])
+        .pipe(gulp.dest('public/stylesheets/scss'))
+        .on('finish', done);
+      });
+  });
+});
+
+// generate font files
+gulp.task("handleFonts", done => {
+  iconStream
+    .pipe(gulp.dest('iconFactory/result/fonts/hiStyle'))
+    .on('finish', () => {
+      gulp.src(['iconFactory/result/fonts/hiStyle/*.*'])
+      .pipe(gulp.dest('public/stylesheets/fonts/hiStyle'))
+      .on('finish', done);
+    });
+});
+
+gulp.task('svg', gulp.parallel("handleGlyphs", "handleFonts"));
+
